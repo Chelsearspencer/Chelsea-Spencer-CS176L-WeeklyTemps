bd372be42de92a7926f98f1164f33338d784a0c2
Chelsearspencer <71734078+Chelsearspencer@users.noreply.github.com>
2021-11-08 01:03:06 -0500

    Correct weekly temps files!

--- WeeklyTemps.java
+++ WeeklyTemps.java
@@ -2,59 +2,61 @@ package labproject2.prob1;
 
 public class WeeklyTemps {
 	
+	
 	public double getMax(double[] temps) {
-		/**
-		 * TODO: implement getMax
-		 * 
-		 * double max=0.0;
-		 * 
-		 * i.e. for(int i=0;...)
-		 * 	if(temps[i] > max){
-		 * 		max=temps[i];
-		 *   }
-		 * -return max
-		 */
-		return 0.0;
+		
+		 //TODO: implement getMax
+		 
+		 double max=0.0;
+		 
+		 for(int i=0;i < temps.length; i++)
+		if(temps[i] > max){
+		 	max=temps[i];
+		   }
+		return max;
+		
 	}
 	
 	public double getMin(double[] temps) {
-		/**
-		 * TODO: return min value in array
-		 */
-		return 0.0;
+		
+		 double min =0.0;
+		 for(int i=0;i < temps.length; i++)
+			 if(temps[i] < min){ 
+			 }
+		return min;
 	}
 	
 	public String getRange(double[] temps) {
-		/**
-		 * TODO: iterate through and build string
-		 * 
-		 * double min = getMin(temps);
-		 * double max = getMax(temps);
-		 * 
-		 * String range = String.format("%.1f - %.1f", min, max)
-		 * 
-		 * return range;
-		 */
-		return "66.1 - 79.5"; 
+		
+		 //TODO: iterate through and build string
+		 
+		double min = getMin(temps);
+		double max = getMax(temps);
+		 
+		 String range = String.format("%.1f - %.1f", min, max);
+		 
+		 return range;
+		 
+		
 	}
 	
 	public double getMean(double[] temps) {
-		/**
-		 * 
-		 * TODO: iterate through temps, calculate mean
-		 * return mean
-		 * 
-		 * double sum = 0.0;
-		 * for(int i=0, ...) {
-		 * 	sum += temps[i];
-		 * }
-		 * 
-		 * double mean = sum/temps.length;
-		 * 
-		 * return mean;
-		 * 
-		 */
-		return 0.0;
+
+		 //TODO: iterate through temps, calculate mean
+		
+		
+		  double sum = 0.0;
+		  for(int i=0; i < temps.length; i++) {
+			  	sum += temps[i];
+	
+		  }
+		  
+		  double mean = sum/temps.length;
+		  
+		  return mean;
+		  
+		 
+		
 	}
 	
 	/**
@@ -68,18 +70,22 @@ public class WeeklyTemps {
 	 * @return
 	 */
 	public int getExtreme(double[] temps) {
-		/**
-		 * TODO: count elements in extreme range
-		 * 
-		 * int count = 0;
-		 * for(i=0..){
-		 * 	if(...){
-		 * 	count +=1;
-		 *  }
-		 * }
-		 * 
-		 * return count;
-		 */
-		return 0;
+		
+		 // TODO: count elements in extreme range
+		  
+	 int extreme = 0;
+	 for(int i=0;i < temps.length; i++) {
+		if(temps[i]>= 95) 
+		{
+			extreme+=1;
+		 }
+		else if (temps[i] <= 5) 
+		{
+			extreme+=1;
+		}
+	 }
+		  return extreme;
 	}
-}
+	}
+
+
