6f1287a9e2be1114af7aa82c99f5f1b26f338c36
Chelsearspencer <71734078+Chelsearspencer@users.noreply.github.com>
2021-11-08 01:01:13 -0500

    correct weekly temps file

--- TestWeeklyTemps.java
+++ TestWeeklyTemps.java
@@ -1,15 +1,26 @@
 package labproject2.prob1;
 
+import java.util.ArrayList;
+
+
 public class TestWeeklyTemps {
 
 	public static void main(String[] args) {
-		double[] testTemps = {32.5, 88.3, 0.3, 100.3, 56.3, 23.5, 99.3};
+		// TODO Auto-generated method stub
+		
+//ArrayList <WeeklyTemps> testTemps = new ArrayList<WeeklyTemps>();	
+
+double[] testTemps = {32.5, 88.3, 0.3, 100.3, 56.3, 23.5, 99.3};
 		
 		WeeklyTemps lastWeek = new WeeklyTemps();
-		System.out.println("");
-		System.out.println(lastWeek.getMean(testTemps));
-
 		
+		System.out.println("weekly temperature data(degrees F):");
+		System.out.println("mean:    "+lastWeek.getMean(testTemps));
+		
+		System.out.println("range:   "+lastWeek.getRange(testTemps));
+		
+		System.out.println("extreme temp days:   "+lastWeek.getExtreme(testTemps));
+
 	}
 
 }
